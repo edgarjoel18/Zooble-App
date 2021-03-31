0fe37d4afc1d2b2fa50c8cf362e741db163cd3cd
Sabrina Dang <sdang4@mail.sfsu.edu>
2021-03-31 03:45:33 -0700

    Added SiteDemo1

--- application/frontend/src/components/SiteDemo1.js
+++ application/frontend/src/components/SiteDemo1.js
@@ -1,11 +1,50 @@
 import React from "react";
+import './Home.css';
+import testImage from '../images/testImages/vertical-testImage.jpg';
+
 
 function SiteDemo1() {
   return (
-    <div>
-        SiteDemo1
+    <div className='site-demo1-container'>
+      <div className='site-demo1-leftSideBox'>
+        <p className='site-demo1-textBox'> Build a profile and connect 
+        <br/>with other pet owners <br/>
+        <br/>OR <br/>
+        <br/>Find a pet by connecting with
+        <br/>a local shelter
+        </p>
+      </div>
+
+
+{/* Image Column 1 for site-demo1 */}
+      <div className = 'site-demo1-columnContainer'>
+        <div className='site-demo1-column'>
+          <div className='site-demo1-columnCard'>
+          <img className='site-demo2-container-img' src={testImage}/>
+          </div>
+        </div>
+      </div>
+
+{/* Image Column 2 for site-demo1 */}
+      <div className = 'site-demo1-columnContainer'>
+        <div className='site-demo1-column'>
+          <div className='site-demo1-columnCard'>
+          <img className='site-demo2-container-img' src={testImage}/>
+
+          </div>
+
+
+        </div>
+      </div>
+
+
     </div>
+    
+
+    
+
   );
 }
+
 
 export default SiteDemo1;
