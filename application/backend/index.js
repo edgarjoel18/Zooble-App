3596b733a1522705439ce854286ff46601fee938
dannyr.simpson@gmail.com <daniel@mail.example.com>
2021-03-31 18:27:30 -0700

    added dropdown menu for categories

--- application/backend/index.js
+++ application/backend/index.js
@@ -25,31 +25,103 @@ app.get("/search", (req,res) =>{
     console.log("/search");
     var name = req.query.searchTerm.toLowerCase();
     console.log(name);
+    var category = req.query.searchCategory
+    console.log(category);
     var requestedSearchResults = {searchResults:[]}
 
-    connection.query(`SELECT * FROM Pet WHERE LOWER(name) LIKE '%${name}%'`, function(err, result) {
-        if(err){
-            throw err;
-        } else {
-          
-            // [ TextRow { pet_id: 1, name: 'Max', size_id: 1, age_id: 1 } ]
-            Object.keys(result).forEach(function(key) {
-                var row = result[key];
-                // console.log(row);
-                // console.log(row.name);
-                // console.log(row.size_id);
-                // console.log(row.age_id);
-                requestedSearchResults.searchResults.push({
-                    "pet_id":row.pet_id,
-                    "name": row.name,
-                    "size_id": row.size_id,
-                    "age_id": row.age_id
-                });
-              });
-            console.log(requestedSearchResults);
-            res.json(requestedSearchResults);
-        }
-    });
+    if(category == 'Pet'){
+        connection.query(
+            `SELECT * 
+            FROM ((Pet
+            INNER JOIN Age
+            ON Pet.age_id = Age.age_id)
+            INNER JOIN Size
+            ON Pet.size_id = Size.size_id)
+            WHERE LOWER(name) LIKE '%${name}%'
+            `, 
+            function(err, result) {
+            if(err){
+                throw err;
+            } else {
+              
+                // [ TextRow { pet_id: 1, name: 'Max', size_id: 1, age_id: 1 } ]
+                Object.keys(result).forEach(function(key) {
+                    var row = result[key];
+                    // console.log(row);
+                    // console.log(row.name);
+                    // console.log(row.size_id);
+                    // console.log(row.age_id);
+                    requestedSearchResults.searchResults.push({
+                        "pet_id":row.pet_id,
+                        "name": row.name,
+                        "size_name": row.size_name,
+                        "age_name": row.age_name
+                    });
+                  });
+                console.log(requestedSearchResults);
+                res.json(requestedSearchResults);
+            }
+        });
+    }
+    else if(category == 'Business'){
+        connection.query(
+            `SELECT * 
+            FROM RegisteredBusiness
+            WHERE LOWER(name) LIKE '%${name}%'
+            `, 
+            function(err, result) {
+            if(err){
+                throw err;
+            } else {
+              
+                // [ TextRow { pet_id: 1, name: 'Max', size_id: 1, age_id: 1 } ]
+                Object.keys(result).forEach(function(key) {
+                    var row = result[key];
+                    // console.log(row);
+                    // console.log(row.name);
+                    // console.log(row.size_id);
+                    // console.log(row.age_id);
+                    requestedSearchResults.searchResults.push({
+                        "reg_business_id":row.reg_business_id,
+                        "reg_user_id":row.reg_user_id,
+                        "name": row.name
+                    });
+                  });
+                console.log(requestedSearchResults);
+                res.json(requestedSearchResults);
+            }
+        });
+    }
+    else if(category == 'Shelter'){
+        connection.query(
+            `SELECT * 
+            FROM RegisteredShelter
+            WHERE LOWER(name) LIKE '%${name}%'
+            `, 
+            function(err, result) {
+            if(err){
+                throw err;
+            } else {
+              
+                // [ TextRow { pet_id: 1, name: 'Max', size_id: 1, age_id: 1 } ]
+                Object.keys(result).forEach(function(key) {
+                    var row = result[key];
+                    // console.log(row);
+                    // console.log(row.name);
+                    // console.log(row.size_id);
+                    // console.log(row.age_id);
+                    requestedSearchResults.searchResults.push({
+                        "reg_shelter_id":row.reg_shelter_id,
+                        "reg_user_id":row.reg_user_id,
+                        "name": row.name
+                    });
+                  });
+                console.log(requestedSearchResults);
+                res.json(requestedSearchResults);
+            }
+        });
+    }
+
 
 });
 
--- application/frontend/src/components/SearchBar.js
+++ application/frontend/src/components/SearchBar.js
@@ -6,11 +6,16 @@ import './SearchBar.css'
 
 function SearchBar() {
   const [searchTerm, setSearchTerm] = useState('')
+  const [searchCategory, setSearchCategory] = useState('Pet');
   const [overlayDisplay, setOverlayDisplay] = useState('none');
   const [recievedSearchResults, setRecievedSearchResults] = useState([])
   
   function OnClickHandler(e){
-    Axios.get('/search',{params: {searchTerm}}).then(response =>{
+    Axios.get('/search',{
+      params: {
+        searchTerm: searchTerm,
+        searchCategory:searchCategory}})
+      .then(response =>{
       // console.log(response)
       // console.log(response.data)
       // console.log(response.data.searchResults)
@@ -29,12 +34,29 @@ function SearchBar() {
         <input type="text" placeholder="Search" onChange={e => setSearchTerm(e.target.value)} />
         <button onClick={OnClickHandler} >Search</button>
       </span>
+      <span className="search-category-dropdown">
+        {searchCategory}
+        <select name="search-category" id="search-category" onChange= {e => setSearchCategory(e.target.value)}>
+          <option value="Pet">Pets</option>
+          <option value="Business">Businesses</option>
+          <option value="Shelter">Shelters</option>
+        </select>
+      </span>
       <div style={overlayStyle} className="search-results-overlay">
         <ul>
-          {recievedSearchResults &&
+          {recievedSearchResults && searchCategory == 'Pet' &&
            recievedSearchResults.map((searchResult) => (
-              <li key={searchResult.pet_id}>{searchResult.name}</li>
+              <li key={searchResult.pet_id}>{searchResult.name} {searchResult.size_name} {searchResult.age_name}</li>
           ))}
+          {recievedSearchResults && searchCategory == 'Business' &&
+           recievedSearchResults.map((searchResult) => (
+              <li key={searchResult.pet_id}>{searchResult.name} {searchResult.reg_business_id} {searchResult.reg_user_id}</li>
+          ))}
+          {recievedSearchResults && searchCategory == 'Shelter' &&
+           recievedSearchResults.map((searchResult) => (
+              <li key={searchResult.pet_id}>{searchResult.name} {searchResult.reg_shelter_id} {searchResult.reg_user_id}</li>
+          ))}
+
         </ul>
         <button onClick= {() => {
                       setOverlayDisplay('none');
