66de1ee35dbdb7ed8922b6e4084ec3f5cd309b38
Cameron Harte <cameron.j.harte@gmail.com>
2021-04-01 00:39:41 -0700

    Hero Container is ready to be review and tested

--- application/frontend/src/components/HeroContainer.js
+++ application/frontend/src/components/HeroContainer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import './HeroContainer.css';
 
 function HeroContainer() {
   return (
-    <div>
-        HeroContainer
+    <div className='hero-container'>
+      <div className='left-side'> 
+        <img className='left-side-img'/> 
+      </div>
+      <div className='right-side'>
+        <p className='right-side-text'>The Social Network for furry (or not so furry) friends</p>
+        <div id='button-container'>
+          <button className='signup-button'>
+            <span>Sign Up</span>
+          </button>
+          <button className='find-Friend-button'>
+            <span>Find a Friend</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
