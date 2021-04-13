a74cd2734845b45106a4da0be9bedcbfeec6093e
babyaced <dannyr.simpson@gmail.com>
2021-04-13 14:52:22 -0700

    added sort dropdown to map search page

--- application/frontend/src/pages/MapSearch/MapSearch.js
+++ application/frontend/src/pages/MapSearch/MapSearch.js
@@ -2,8 +2,11 @@ import {React, useState} from 'react'
 
 import styles from './MapSearch.module.css'
 
+import DropdownIcon from '../../images/Created Icons/Dropdown.svg'
+
 function MapSearch() {
     const[searchCategory, setSearchCategory] = useState('Pet');
+    const[resultsSortOption, setResultsSortOption] = useState('');
     const[recievedSearchResults, setRecievedSearchResults] = useState([ 
         {   pet_id: 1,
             name: 'Max',
@@ -49,20 +52,23 @@ function MapSearch() {
             <div className={styles['map-search-results-map']}>Map</div>
             <div className={styles['map-search-results-text']}>
                 <div className={styles['map-search-results-header']}><h1 className={styles['map-search-results-header-text']}>Results</h1>
-
-                {/* <select name="search-category" id="search-category" onChange= {e => setSearchCategory(e.target.value)}>
-                        <option value="Pet">Pets</option>
-                        <option value="Business">Businesses</option>
-                        <option value="Shelter">Shelters</option>
-                </select> */}
+                <div className={styles['sort-dropdown']}>
+                    <span><h5>Sort By:</h5></span> 
+                <select name="search-category" id="search-category" onChange= {e => setResultsSortOption(e.target.value)}>
+                        <option value="Account Age">Newly Added</option>
+                        <option value="Distance">Distance</option>
+                </select>
+                <img src={DropdownIcon}/>
+                </div>                
                 </div>
-
+                <div className={styles['map-search-results-text-list']}>
                 <ul>
                 {recievedSearchResults.length == 0 && <li>No Results</li>}
                 {recievedSearchResults && searchCategory == 'Pet' && recievedSearchResults.map((searchResult) => (
                     <li key={searchResult.pet_id}><img src={searchResult.profile_pic}/><span><h3>{searchResult.name}</h3></span></li>
                 ))}
                 </ul>
+                </div>
             </div>
         </div>
         </>
