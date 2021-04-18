a5e6e5afcec4cb60598f9f80e7ee7255bace5e18
wye-ts <you@example.com>
2021-04-17 19:48:36 -0700

    small fix on resizing

--- application/frontend/src/components/ProfileInfo/ProfileInfo.js
+++ application/frontend/src/components/ProfileInfo/ProfileInfo.js
@@ -61,7 +61,7 @@ function ProfileInfo(props) {
                         onChange= {event => setFollow(event.target.value)}
                     >
                         <option value="follow" >Follow</option>
-                        <option value="unfollow" >Followers</option>
+                        <option value="followers" >Followers</option>
                     </select>
                     {!props.isSelfView && <button className={styles.Button} >Message</button>}
                 </div>
