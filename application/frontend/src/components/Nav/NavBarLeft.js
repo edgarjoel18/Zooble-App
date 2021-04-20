ffe59a195d6506463907dc14a512b14af6e8fc44
babyaced <dannyr.simpson@gmail.com>
2021-04-19 17:57:04 -0700

    added cool logo as img to navbar

--- application/frontend/src/components/Nav/NavBarLeft.js
+++ application/frontend/src/components/Nav/NavBarLeft.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import LogoPng from '../../images/ZoobleLogo.png';
+import LogoPng from '../../images/Created Icons/Logo.png';
 
 import styles from './NavBar.module.css'
 
