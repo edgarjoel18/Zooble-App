02d8c4a6f4b6016c6f1ed5bfb00a598b3cd7fba1
babyaced <dannyr.simpson@gmail.com>
2021-04-22 01:11:53 -0700

    added required specifier to fields in view message and send message

--- application/frontend/src/components/Modals/SendAMessage.js
+++ application/frontend/src/components/Modals/SendAMessage.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 import Modal from './Modal.js'
 
 import styles from './SendAMessage.module.css';
 
 function SendAMessage({display,onClose}) {
+
+    const [sendSuccess,setSendSuccess] = useState(false);
+
     if(!display) return null
     return (
         <Modal display={display} onClose={onClose}>
+            <>
             <h1 className={styles["sendAMessage-header"]}>Send a Message</h1>
-            <div className={styles['send-a-message-container']}>
-                <input className={styles["sendAMessage-subject"]} placeholder="Subject"/>
-                <textarea className={styles["sendAMessage-body"]}/>
-                <button class={styles["sendAMessage-sendButton"]} onClick={onClose}><h4>Send</h4></button>
-            </div>
+                <form className={styles['send-a-message-container']}>
+                    <input className={styles["sendAMessage-subject"]} placeholder="Subject" required/>
+                    <textarea className={styles["sendAMessage-body"]} required placeholder="Write your message here"/>
+                    <button type="submit" class={styles["sendAMessage-sendButton"]}><h4>Send</h4></button>
+                </form>
+            </>
         </Modal>
     )
 }
