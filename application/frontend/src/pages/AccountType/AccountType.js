761c7f0d52f17554f6f3754f76e58c92fa3186d0
babyaced <dannyr.simpson@gmail.com>
2021-04-26 18:57:25 -0700

    changed hover color on account type page to navy for better contrast

--- application/frontend/src/pages/AccountType/AccountType.js
+++ application/frontend/src/pages/AccountType/AccountType.js
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './AccountType.module.css';
 
@@ -7,6 +8,7 @@ import ShelterImage from '../../images/Third Party Images/undraw_pet_adoption_2q
 import BusinessImage from '../../images/Third Party Images/undraw_business_shop_qw5t.svg'
 
 function AccountTypePage() {
+
     return (
         <div className={styles['account-type-container']}>
                     <div className={styles['owner-container']}>
@@ -16,12 +18,12 @@ function AccountTypePage() {
                         </NavLink>
                     </div>
                     <div className={styles['shelter-container']}>
-                        <img className={styles['shelter-img']} src={ShelterImage} />
+                        <img className={styles['shelter-img']} src={ShelterImage}/>
                         <NavLink to="/shelter-signup">
                             <button className={styles['shelter-button']}>Sign Up as Pet Shelter</button>
                         </NavLink>
                     </div>
-                    <div className={styles['business-container']}>
+                    <div className={styles['business-container'+'']}>
                         <img className={styles['business-img']} src={BusinessImage} />
                         <NavLink to="/business-signup">
                             <button className={styles['business-button']}>Sign Up as Pet Business</button>
