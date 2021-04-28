ce83f0abce79e59224888f437e725e5c266c8edc
babyaced <dannyr.simpson@gmail.com>
2021-04-28 10:49:03 -0700

    made fields in message modals required

--- application/frontend/src/components/Modals/SendAMessage.js
+++ application/frontend/src/components/Modals/SendAMessage.js
@@ -12,11 +12,11 @@ function SendAMessage({display,onClose}) {
         <Modal display={display} onClose={onClose}>
             <>
             <h1 className={styles["sendAMessage-header"]}>Send a Message</h1>
-                <div className={styles['send-a-message-container']}>
+                <form className={styles['send-a-message-container']} onSubmit={onClose}>
                     <input className={styles["sendAMessage-subject"]} placeholder="Subject" required/>
                     <textarea className={styles["sendAMessage-body"]} required placeholder="Write your message here"/>
-                    <button type="submit" class={styles["sendAMessage-sendButton"]} onClick={onClose}><h4>Send</h4></button>
-                </div>
+                    <button type="submit" class={styles["sendAMessage-sendButton"]} ><h4>Send</h4></button>
+                </form>
             </>
         </Modal>
     )
