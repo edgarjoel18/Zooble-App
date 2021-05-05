5913792d585ccb76c8347f4b233901a7411828c3
babyaced <dannyr.simpson@gmail.com>
2021-05-05 15:20:59 -0700

    added tag pet dropdown, restyled drag and drop area

--- application/frontend/src/pages/Feed/Feed.js
+++ application/frontend/src/pages/Feed/Feed.js
@@ -1,15 +1,16 @@
 import { useState, useEffect, useCallback } from 'react'
 import { NavLink, useHistory } from "react-router-dom";
+import {useDropzone} from 'react-dropzone'
+import axios from 'axios';
+import Select from 'react-select';  
+
 import styles from './Feed.module.css'
-import bus_prof_pic from '../../images/businessProfile.jpg'
-import shel_prof_pic from '../../images/shelterProfile.jpg'
-import own_prof_pic from '../../images/petOwnerProfile.jpg'
+
 import PostModal from '../../components/Modals/PostModal'
 
-import {useDropzone} from 'react-dropzone'
 
-import ArrowIcon from '../../images/Created Icons/Arrow.svg'
-import axios from 'axios';
+
+
 
 // import ClipLoader from "react-spinners/ClipLoader";
 
@@ -19,46 +20,44 @@ const apiGatewayURL = 'https://5gdyytvwb5.execute-api.us-west-2.amazonaws.com/de
 function Feed() {
 
     const [postModalDisplay, setPostModalDisplay] = useState(false);
-    const [feedPosts, setFeedPosts] = useState([
-        // {
-        //     post_id: 1,
-        //     user_display_name: 'Paw Spa',
-        //     link: "/Profile/BusinessId=2",
-        //     profile_pic: 'https://csc648groupproject.s3-us-west-2.amazonaws.com/PawSpaPic.jpg',
-        //     pic: 'https://csc648groupproject.s3-us-west-2.amazonaws.com/MimiPic.jpg',
-        //     likes: 0,
-        //     timestamp: '12/25/20 at 11:05 AM',
-        //     body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,'
-        // },
-        // {
-        //     post_id: 2,
-        //     user_display_name: 'Burgsdale Pet Shelter',
-        //     link: "/Profile/ShelterId=2",
-        //     profile_pic: 'https://csc648groupproject.s3-us-west-2.amazonaws.com/BurgsdalePetShelterPic.jpg',
-        //     pic: 'https://csc648groupproject.s3-us-west-2.amazonaws.com/MaxPic.jpg',
-        //     likes: 10,
-        //     timestamp: '12/25/20 at 11:05 AM',
-        //     body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,'
-        // },
-        // {
-        //     post_id: 3,
-        //     user_display_name: 'Alex',
-        //     link: "/Profile/PetOwnerId=1",
-        //     profile_pic: 'https://csc648groupproject.s3-us-west-2.amazonaws.com/AlexPic.jpg',
-        //     pic: 'https://csc648groupproject.s3-us-west-2.amazonaws.com/JujuPic.jpg',
-        //     likes: 20,
-        //     timestamp: '12/25/20 at 11:05 AM',
-        //     body: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,'
-        // }
-    ]);
+    const [feedPosts, setFeedPosts] = useState([]);
 
     //creating a post display
     const [createPostDisplayName, setCreatePostDisplayName] = useState('');
     const [createPostProfilePic, setCreatePostProfilePic] = useState('');
+    const [createdPostBody, setCreatedPostBody] = useState();
 
+    //selectedPost to pass to post modal
     const [selectedPost, setSelectedPost] = useState({});
 
-    const [createdPostBody, setCreatedPostBody] = useState();
+    //storing the pets available to tag in the dropdown menu
+    const [taggablePets, setTaggablePets] = useState([]);
+
+    //storing the pets that are tagged in each post to send to db
+    const [taggedPets, setTaggedPets] = useState([]);
+
+    //image upload array
+    const [myFiles, setMyFiles] = useState([])
+
+    function customTheme(theme) { //move this a separate file and import maybe?
+        return {
+            ...theme,
+            colors: {
+                ...theme.colors,
+                primary25: '#B3B3B3',
+                primary: '#1CB48F',
+            }
+        }
+    }
+
+    const customStyles = {
+        control: (base, state) => ({
+          ...base,
+          height: '54.5px',
+          'min-height': '54.5px',
+          'border-radius': '7.5px',
+        }),
+    };
 
 
     //runs on refresh
@@ -82,23 +81,33 @@ function Feed() {
             console.log(response.data);
             setFeedPosts(response.data);
         })
-    }, [])
+        .catch(err =>{
+            console.log("Error: ");
+            console.log(err);
+        })
 
-    //runs whenever the user creates a post
-    useEffect(()=>{
-        console.log('/api/get-feed-posts');
-        axios.get('/api/get-feed-posts')
+        axios.get('/api/get-current-user-pets')
         .then(response =>{
             console.log(response.data);
-            setFeedPosts(response.data);
+            setTaggablePets(response.data);
         })
-    },[])
+    }, [])
+
+    // //runs whenever the user creates a post
+    // useEffect(()=>{
+    //     console.log('/api/get-feed-posts');
+    //     axios.get('/api/get-feed-posts')
+    //     .then(response =>{
+    //         console.log(response.data);
+    //         setFeedPosts(response.data);
+    //     })
+    // },[])
 
 
 
 
 
-    const [myFiles, setMyFiles] = useState([])
+    
 
     useEffect(() => () => {
         console.log('revoking object urls');
@@ -208,6 +217,16 @@ function Feed() {
                     <img className={styles["follower-feed-new-post-pic"]} src={createPostProfilePic} />
                     <div className={styles["follower-feed-new-post-name"]}>{createPostDisplayName}</div>
                     <textarea value={createdPostBody} maxLength="255" required className={styles["follower-feed-new-post-body"]} placeholder="Update your followers on what's going on with you and your pets"  onChange={e => setCreatedPostBody(e.target.value)}/>
+                    <div className={styles['follower-feed-new-post-tag-dropdown']}>
+                        <Select
+                            onChange={setTaggedPets}
+                            options={taggablePets}
+                            placeholder="Tag a Pet in your Post"
+                            theme={customTheme}
+                            styles={customStyles}
+                            isSearchable
+                        />
+                    </div>
                     <section className={styles["follower-feed-new-post-attach-image"]}>
                         <div className={styles["follower-feed-new-post-attach-image-container"]}  {...getRootProps()}>
                             <input  {...getInputProps()} />
