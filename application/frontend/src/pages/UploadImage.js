791f1c34ab5c898028f7b1c1d9710fb507ef2990
babyaced <dannyr.simpson@gmail.com>
2021-05-04 21:17:11 -0700

    image upload working (no styling/integration/file limits)

--- application/frontend/src/pages/UploadImage.js
+++ application/frontend/src/pages/UploadImage.js
@@ -1,20 +1,33 @@
-import {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import {useCallback, useState} from 'react'
+import Dropzone from 'react-dropzone'
 
 import axios from 'axios'
 
 const apiGatewayURL = 'https://5gdyytvwb5.execute-api.us-west-2.amazonaws.com/default/getPresignedURL'
 
-function UploadImage() {
 
-    const onDrop = useCallback(acceptedFiles => {
+function UploadImage() {
+    
+
+    const [image, setImage] = useState('https://csc648groupproject.s3-us-west-2.amazonaws.com/DefaultProfilePic.svg');
+
+    const onDrop = (acceptedFiles) => {
+        let config = {
+            headers: {
+                'Content-type': 'image/jpeg'
+            }
+        }
         // Do something with the files
         axios.get(apiGatewayURL)  //first get the presigned s3 url
         .then((response) =>{
             console.log(response)
             console.log(response.data)
-            axios.put(response.data.uploadURL, acceptedFiles[0]).then((response) =>{
+            let presignedFileURL = response.data.photoFilename;
+            console.log(acceptedFiles[0]);
+            axios.put(response.data.uploadURL, acceptedFiles[0],config).then((response) =>{
                 console.log(response);
+                console.log(response.data);
+                setImage('https://csc648groupproject.s3-us-west-2.amazonaws.com/' + presignedFileURL);
             })
             .catch((err) =>{
                 console.log(err);
@@ -23,20 +36,23 @@ function UploadImage() {
         .catch((err) =>{
             console.log(err);
         })
-      }, [])
-    
-    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+      }
 
 
     return (
-        <div {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-            isDragActive ?
-                <p>Drop the files here ...</p> :
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            }
-        </div>
+        <>
+            <Dropzone onDrop={onDrop} multiple={false} maxSize={0}>
+                {({getRootProps, getInputProps}) => (
+                    <section>
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>Drag 'n' drop some files here, or click to select files</p>
+                        </div>
+                    </section>
+                )}
+            </Dropzone>
+            <img src={image}/>
+        </>
     )
 }
 
