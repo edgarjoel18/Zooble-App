bd210c530c36fb3939279f721734d1593a488b3d
Edgar Catalan <edgar@Edgars-MacBook-Pro.local>
2021-05-06 17:37:09 -0700

    first commit

--- application/backend/index.js
+++ application/backend/index.js
@@ -1,54 +1,55 @@
 const express = require("express");
 
-const session = require('express-session');
+const session = require("express-session");
 
+// A change
 //DB Connection
-const connection = require('./db');
+const connection = require("./db");
 
 //Routes
-const loginRouter = require('./routes/login.js');
-const logoutRouter = require('./routes/logout.js');
-const signupRouter = require('./routes/sign-up.js');
-const searchRouter = require('./routes/search.js');
-const dropdownRouter = require('./routes/dropdown-options.js');
-const uploadPostRouter = require('./routes/upload-post.js');
-const editPostRouter = require('./routes/edit-post.js');
-const deletePostRouter = require('./routes/delete-post.js');
-const feedRouter = require('./routes/feed.js');
-const commentsRouter = require('./routes/comments.js');
-const followUserRouter = require('./routes/follow-unfollow-user.js');
-const petsRouter = require('./routes/pets.js');
-const likeRouter = require('./routes/like.js')
-const profileRouter = require('./routes/profile.js');
+const loginRouter = require("./routes/login.js");
+const logoutRouter = require("./routes/logout.js");
+const signupRouter = require("./routes/sign-up.js");
+const searchRouter = require("./routes/search.js");
+const dropdownRouter = require("./routes/dropdown-options.js");
+const uploadPostRouter = require("./routes/upload-post.js");
+const editPostRouter = require("./routes/edit-post.js");
+const deletePostRouter = require("./routes/delete-post.js");
+const feedRouter = require("./routes/feed.js");
+const commentsRouter = require("./routes/comments.js");
+const followUserRouter = require("./routes/follow-unfollow-user.js");
+const petsRouter = require("./routes/pets.js");
+const likeRouter = require("./routes/like.js");
+const profileRouter = require("./routes/profile.js");
 
 const app = express();
 
 // const cors = require('cors');
 
 //Session Store
-const sessionStore = require('./session-store');
-
-
+const sessionStore = require("./session-store");
 
 // app.use(cookieParser());
 
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(session({
-     key: "userId",
-     secret: "zooble",
-     resave: false,
-     saveUninitialized: false,
-     store: sessionStore,
-     cookie: {
-         expires: 60 * 60 * 24 * 1000, //1 day expiration
-     }
- }))
+app.use(
+  session({
+    key: "userId",
+    secret: "zooble",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      expires: 60 * 60 * 24 * 1000, //1 day expiration
+    },
+  })
+);
 
 //start express server on port 5000
-app.listen(5000, () =>{
-    console.log("server started on port 5000");
+app.listen(5000, () => {
+  console.log("server started on port 5000");
 });
 
 //Sign Up
@@ -64,23 +65,22 @@ app.use(logoutRouter);
 app.use(searchRouter);
 
 //Dropdown Options
-app.use(dropdownRouter)
+app.use(dropdownRouter);
 
 //Upload Post
-app.use(uploadPostRouter)
+app.use(uploadPostRouter);
 
 //Edit Post
-app.use(editPostRouter)
+app.use(editPostRouter);
 
 //Delete Post
-app.use(deletePostRouter)
+app.use(deletePostRouter);
 
 //Feed
 app.use(feedRouter);
 
 //Insert, Get Comments
 app.use(commentsRouter);
-
 
 // Follow a user
 app.use(followUserRouter);
@@ -89,19 +89,7 @@ app.use(followUserRouter);
 app.use(petsRouter);
 
 //Liking/unliking a post
-app.use(likeRouter)
+app.use(likeRouter);
 
 //Profile
-app.use(profileRouter)
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(profileRouter);
