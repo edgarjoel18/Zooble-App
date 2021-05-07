7c11ecd734d5220881b77848255f4c034eeb8c13
babyaced <dannyr.simpson@gmail.com>
2021-05-07 16:26:17 -0700

    messages and add a pet initial versions implemented on backend

--- application/backend/routes/pet-profile.js
+++ application/backend/routes/pet-profile.js
@@ -26,16 +26,16 @@ router.post("/api/create-pet-profile",(req,res)=>{
                  console.log(err);
              }
              else{
-                 console.log("Inserted Pet successfully");
+                console.log("Inserted Pet successfully");
                 console.log(userPet);
                  connection.query(
                      `INSERT INTO Profile
                       (display_name,about_me, account_id, pet_id)
-                      VALUES (req.body.name, '', 
+                      VALUES ('${req.body.name}', '', 
                       (SELECT Account.account_id
                         FROM Account
                         JOIN RegisteredUser ON RegisteredUser.reg_user_id = '${req.session.reg_user_id}'
-                        JOIN Account ON Account.user_id = RegisteredUser.user_id),
+                        WHERE Account.user_id = RegisteredUser.user_id),
                       '${userPet.insertId}')`,
                       function(err, result){
                           if(err){
@@ -43,75 +43,90 @@ router.post("/api/create-pet-profile",(req,res)=>{
                           }
                           else{
                               console.log(result);
-                          }
-                      }
-                 )
+                              for(let i = 0; i < req.body.color.length; i++){
+                                connection.query(`INSERT INTO PetColor (pet_id,color_id) VALUES ('${userPet.insertId}','${req.body.color[i].value}')`,
+                                    function(err, result){
+                                        if(err){
+                                            console.log(err);
+                                        //  res.status(500).json(err);
+                                        //  res.end();
+                                        }
+                                        else{
+                                            console.log(req.body.color[i].label + " inserted")
+                                        }
+                                    }
+                                )
+                                }
 
-                
-                for(let i = 0; i < req.body.color.length; i++){
-                    connection.query(
-                        `INSERT INTO PetColor (pet_id,color_id) VALUES ('${userPet.insertId}','${req.body.color[i].value}')`,
-                         function(err, result){
-                             if(err){
-                                 console.log(err);
-                             }
-                             else{
-                                 if(req.body.dogBreed.length != 0 && req.body.petType.label == 'Dog'){
+                                if(req.body.dogBreed.length !== 0 && req.body.type.label == 'Dog'){
                                     connection.query(`INSERT INTO Dog (pet_id) VALUES ('${userPet.insertId}')`,
                                     function(err, insertedDog){
                                         if(err){
                                             console.log(err);
+                                            // res.status(500).json(err);
                                         }
                                         else{
                                             console.log("Inserted Dog successfully");
                                             console.log(insertedDog);
                                             for(let i = 0; i < req.body.dogBreed.length; i++){
-                                                connection.query(`INSERT INTO DogBreeds (dog_id, dog_breed_id) VALUES ('${insertedDog.insertId}','${req.body.dogBreed[0]}')`,
+                                                connection.query(`INSERT INTO DogBreeds (dog_id, breed_id) VALUES ('${insertedDog.insertId}','${req.body.dogBreed[i].value}')`,
                                                     function(err,insertedDogBreed){
                                                         if(err){
                                                             console.log(err);
+                                                            // res.status(500).json(err);
                                                         }
                                                         else{
-                                                            console.log(req.body.dogBreed[i].label, " inserted");
+                                                            console.log("Dog Breed: ", req.body.dogBreed[i].label, " inserted");
                                                         }
                                                 })
                                             }
+                                            // res.status(200).json(insertedDog);
                                         }
                                     })
-
-                                 }
-                                 else if(req.body.catBreed.length != 0 && req.body.petType.label == 'Cat'){
+                                }
+                                else if(req.body.catBreed.length !== 0 && req.body.type.label == 'Cat'){
                                     connection.query(`INSERT INTO Cat (pet_id) VALUES ('${userPet.insertId}')`,
                                     function(err, insertedCat){
                                         if(err){
                                             console.log(err);
+                                            // res.status(500).json(err);
                                         }
                                         else{
                                             console.log("Inserted Cat successfully");
                                             console.log(insertedCat);
                                             for(let i = 0; i < req.body.catBreed.length; i++){
-                                                `INSERT INTO CatBreeds (cat_id, cat_breed_id) VALUES ('${insertedDog.insertId}','${req.body.dogBreed[0]}')`,
-                                                function(err,insertedDogBreed){
+                                                connection.query(`INSERT INTO CatBreeds (cat_id, breed_id) VALUES ('${insertedCat.insertId}','${req.body.catBreed[i].value}')`,
+                                                function(err,insertedCatBreed){
                                                     if(err){
                                                         console.log(err);
                                                     }
                                                     else{
-                                                       console.log(req.body.dogBreed[i].label, " inserted");
+                                                        console.log("Cat Breed: ",req.body.catBreed[i].label, " inserted");
                                                     }
-                                                }
+                                                })
                                             }
+                                            // res.status(200).json(insertedCat);
+                                            // res.end();
                                         }
                                     })
-                                 }
-                                 else{
-                                 }
-                             }
-                         }
-                    )
-                 }
-             }
-             res.status(200).json(userPet);
-         })
+                                }
+                                else{
+                                    connection.query(`INSERT INTO OtherPet (pet_id, type_id) VALUES ('${userPet.insertId}','${req.body.type.value}')`,
+                                    function(err, insertedOtherPet){
+                                        if(err){
+                                            console.log(err);
+                                        }
+                                        else{
+                                            console.log(insertedOtherPet);
+                                            // res.status(200).json(insertedOtherPet);
+                                        }
+                                    })
+                                }
+                          }
+                    })
+                }
+    })
+    // res.status(200).json(userPet);
 })
 
 module.exports = router
