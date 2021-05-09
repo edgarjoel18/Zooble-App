7c27ecdf796989679ce4cdbc1befa8b403d59b08
babyaced <dannyr.simpson@gmail.com>
2021-05-08 18:57:00 -0700

    cookie now correctly cleared on logout

--- application/backend/index.js
+++ application/backend/index.js
@@ -51,6 +51,7 @@ app.use(session({
      }
  }))
 
+
 //start express server on port 5000
 app.listen(5000, () =>{
     console.log("server started on port 5000");
--- application/backend/routes/logout.js
+++ application/backend/routes/logout.js
@@ -5,7 +5,12 @@ const connection = require('../db');
 
 router.post("/api/logout",(req,res) =>{
     console.log("/api/logout")
-    req.session.destroy();
+    req.session.destroy(err =>{
+        if(err){
+            console.log(err);
+        }
+    })
+    res.clearCookie("userId");
     res.send({loggedIn:false})
 })
 
