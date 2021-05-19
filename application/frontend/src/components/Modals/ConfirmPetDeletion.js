7bfcbb6c507c89156238c7fb56024dc5694e76db
babyaced <dannyr.simpson@gmail.com>
2021-05-19 16:18:50 -0700

    added confirm deletion modal to admin feed

--- application/frontend/src/components/Modals/ConfirmPetDeletion.js
+++ application/frontend/src/components/Modals/ConfirmPetDeletion.js
@@ -5,12 +5,12 @@ import styles from './ConfirmPetDeletion.module.css'
 
 import Modal from './Modal'
 
-function ConfirmPetDeletion({display,onClose,selectedPet, deleteAction}) {
+function ConfirmPetDeletion({display,onClose,selectedPet, message, deleteAction}) {
     return (
         <Modal display={display} onClose={onClose}>
             <div className={styles['delete-pet-container']}>
                 <div className={styles['delete-pet-header']}>Are you Sure?</div>
-                <div className={styles['delete-pet-body']}>Delete {selectedPet.name} from your account?</div>
+                <div className={styles['delete-pet-body']}>{message}</div>
                 <button className={styles['delete-pet-confirm-button']} onClick={deleteAction}>Delete</button>
             </div>
         </Modal>
